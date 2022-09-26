2a6fe8923036215dc12771efb14e161b83face05
Paul Pedro <unkerpaulie@gmail.com>
2022-09-26 06:58:05 -0400

    Created medical_app application in project

--- medical/medical/settings.py
+++ medical/medical/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my additional apps
+    'medical_app',
 ]
 
 MIDDLEWARE = [
